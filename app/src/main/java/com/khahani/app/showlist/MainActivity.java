e9b6e12c19aa4722ab7813c4363468ffea683175
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-05 08:23:15 -0700

    استفاده از لیست ویو و اداپتر

--- app/src/main/java/com/khahani/app/showlist/MainActivity.java
+++ app/src/main/java/com/khahani/app/showlist/MainActivity.java
@@ -3,22 +3,35 @@ package com.khahani.app.showlist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
 
-        LinearLayout parent = (LinearLayout)findViewById(R.id.parent);
+        ArrayList<String> numbers = new ArrayList<>();
 
         for (int i = 1; i <= 100; i++) {
-            TextView number = new TextView(this);
-            number.setText(String.valueOf(i));
-
-            parent.addView(number);
+            numbers.add(String.valueOf(i));
         }
+
+        /*ArrayList<String> students = new ArrayList<>();
+        students.add("Khahani");
+        students.add("Javan");
+        students.add("Hasani");
+        students.add("...");*/
+
+        NumbersAdapter adapter = new NumbersAdapter(this, numbers);
+
+        ListView myList  = (ListView)findViewById(R.id.myList);
+
+        myList.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/khahani/app/showlist/NumbersAdapter.java
@@ -0,0 +1,38 @@
+package com.khahani.app.showlist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NumbersAdapter extends ArrayAdapter<String> {
+
+    public NumbersAdapter(@NonNull Context context, @NonNull List<String> numbers) {
+        super(context, 0, numbers);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null){
+            convertView =
+                    LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        // get data
+        String number = getItem(position);
+
+        // retrieve layout textview and set number value
+        TextView numberTextView = (TextView)convertView.findViewById(R.id.number);
+        numberTextView.setText(number);
+
+        return convertView;
+    }
+}
