c1a946e7b80aa48b79efc4f60413eb23f1fe25c0
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-05 07:28:21 -0700

    روش اول - استفاده از لایه ها

---
+++ app/src/main/java/com/khahani/app/showlist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.khahani.app.showlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
