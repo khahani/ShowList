44cc9964aa6d1152a02dfdaa3805ac2ca71770d4
Mohammadreza Khahani <mr.khahani@gmail.com>
2018-07-05 07:44:42 -0700

    استفاده از برنامه نویس جاوا

--- app/src/main/java/com/khahani/app/showlist/MainActivity.java
+++ app/src/main/java/com/khahani/app/showlist/MainActivity.java
@@ -2,12 +2,23 @@ package com.khahani.app.showlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        LinearLayout parent = (LinearLayout)findViewById(R.id.parent);
+
+        for (int i = 1; i <= 100; i++) {
+            TextView number = new TextView(this);
+            number.setText(String.valueOf(i));
+
+            parent.addView(number);
+        }
     }
 }
